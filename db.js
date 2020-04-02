7cd26801e7c394643ce455e383f21712acc49202
tcodling <thomas.codling@outlook.com>
2020-04-03 11:57:22 +1300

    Setup homepage route

--- db.js
+++ db.js
@@ -4,9 +4,11 @@ const connection = require('knex')(config)
 
 //functions
 
-
+function getAlbums(db = connection) {
+  return db('albums').select()
+}
 
 //export functions
 module.exports = {
-  
+  getAlbums
 }
--- routes/router.js
+++ routes/router.js
@@ -4,4 +4,10 @@ const db = require('../db')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    db.getAlbums().then(albums => {
+        res.render('partials/home', {'albums': albums})
+    })
+})
+
 module.exports = router
